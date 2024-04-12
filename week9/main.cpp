9c24724a33ffdb340219f3266840ae9aa74ae8e7
Joseph Vincento <joseph.vincento@gmail.com>
2024-04-12 14:02:30 -0400

    updated main for user input

--- week9/main.cpp
+++ week9/main.cpp
@@ -3,6 +3,30 @@
 
 using namespace std;
 
+void user_input() {
+    double x;
+    double y;
+    double z;
+    Point3d points[3];
+    
+    for(int i = 1; i < 4; i++) {
+        cout << "For point " << i << ", enter a value for x: ";
+        cin >> x;
+        cout << "For point " << i << ", enter a value for y: ";
+        cin >> y;
+        cout << "For point " << i << ", enter a value for z: ";
+        cin >> z;
+
+        points[i - 1] = Point3d(x, y, z);
+    }
+
+    Triangle3d t = Triangle3d(points[0], points[1], points[2]);
+    
+    cout << "For the 3d triangle with points:" << endl;
+    cout << t;
+    cout << "The area of the triangle is " << t.calc_area() << endl;
+}
+
 int main() {
     // Example taken from https://planetcalc.com/218/
     Point3d p1 = Point3d(1, 1, 0);
@@ -14,9 +38,23 @@ int main() {
     t.set_p2(p2);
     t.set_p3(p3);
 
+    cout << "Demo" << endl;
     cout << "For the 3d triangle with points:" << endl;
     cout << t;
-    cout << "The area of the triangle is " << t.calc_area() << endl;;
+    cout << "The area of the triangle is " << t.calc_area() << endl;
+
+    bool cont = true;
+    char in;
+
+    while(cont) {
+        cout << "Try your own? (y/n): ";
+        cin >> in;
+
+        if (in == 'y' || in == 'Y')
+            user_input();
+        else
+            cont = false;
+    }
 
     return 0;
 }
